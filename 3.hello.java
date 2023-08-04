f7fef5f3ccbcc0de4f6180fd00f464ebb1990d86
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-04 21:57:21 +0530

    Update 3.class viewer.java

--- 3.hello.java
+++ 3.hello.java
@@ -1,3 +1,14 @@
-class pavan{
+class viewers{
+	String name;
+	boolean isliked;
+	boolean issubscribed;
+	public static  void main(String[] args){
+	 viewers v1=new viewers();
+	 v1.name="pavan";
+	 v1.isliked=true;
+	 v1.issubscribed=false;
+	System.out.println(v1.name);
+	System.out.println(v1.isliked);
+	System.out.println(v1.issubscribed);
+	}
 
-}
