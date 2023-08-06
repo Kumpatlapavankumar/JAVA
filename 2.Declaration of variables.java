7bd0046f3d03882aed3ceb4f29b76fe2f739bbde
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-06 14:47:33 +0530

    Create 2.Declaration of variables.java

---
+++ 2.Declaration of variables.java
@@ -0,0 +1,14 @@
+class variables{
+ public static void main(String[] args){
+	 int a=10;//integers
+	 float b=10;//decimals ()
+	 double c=10;//decimals()
+	 String s="name";//collection of characters
+	 char ch='s';
+	 System.out.println(a);
+	 System.out.println(b);
+	 System.out.println(c);
+	 System.out.println(s);
+	 System.out.println(ch);
+ }
+}
