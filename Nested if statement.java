ff7c212cd9c8d23f32b6f70d4d63bea47482d1f3
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-14 21:34:34 +0530

    Create Nested if statement.java

---
+++ Nested if statement.java
@@ -0,0 +1,21 @@
+if(condition){    
+     //code to be executed    
+          if(condition){  
+             //code to be executed    
+    }    
+}  
+//example
+/Java Program to demonstrate the use of Nested If Statement.  
+public class JavaNestedIfExample {    
+public static void main(String[] args) {    
+    //Creating two variables for age and weight  
+    int age=20;  
+    int weight=80;    
+    //applying condition on age and weight  
+    if(age>=18){    
+        if(weight>50){  
+            System.out.println("You are eligible to donate blood");  
+        }    
+    }    
+}}  
+
