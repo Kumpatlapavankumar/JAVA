b53b506aee21face5479b955cad677b33543523a
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-15 22:43:28 +0530

    Create for loop.java

---
+++ for loop.java
@@ -0,0 +1,19 @@
+//SYNTAX
+for(initialization; condition; increment/decrement){    
+//statement or code to be executed    
+}    
+
+//example
+
+//Java Program to demonstrate the example of for loop  
+//which prints table of 1  
+public class ForExample {  
+public static void main(String[] args) {  
+    //Code of Java for loop  
+    for(int i=1;i<=10;i++){  
+        System.out.println(i);  
+    }  
+}  
+}  
+
+
