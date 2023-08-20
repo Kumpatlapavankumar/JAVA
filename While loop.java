a593c634c06b7a3a32bbc6ca5425a49a2d0e1023
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-20 23:00:51 +0530

    Create While loop.java

---
+++ While loop.java
@@ -0,0 +1,15 @@
+while (condition){    
+//code to be executed   
+I ncrement / decrement statement  
+}    
+//example
+public class WhileExample {  
+public static void main(String[] args) {  
+    int i=1;  
+    while(i<=10){  
+        System.out.println(i);  
+    i++;  
+    }  
+}  
+}  
+
