f6f94d59b611e4551653e604a7d601a1f8f85a7a
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-23 23:26:23 +0530

    Create Infinitive do-while Loop.java

---
+++ Infinitive do-while Loop.java
@@ -0,0 +1,13 @@
+do{  
+//code to be executed  
+}while(true);  
+//example
+public class DoWhileExample2 {  
+public static void main(String[] args) {  
+    do{  
+        System.out.println("infinitive do while loop");  
+    }while(true);  
+}  
+}  
+
+
