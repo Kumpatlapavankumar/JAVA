5d045b0a92ef60256fae546d8a29250d5de9bd46
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-08-31 23:10:40 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class amstrong{
+public static void main(String[] args){
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int num=n;
+		int sum=0;
+		while(n!=0){
+			int digit=n%10;
+			sum=sum+(digit*digit*digit);
+			n=n/10;
+		}
+		if(num==sum){
+			System.out.println("Amstrong");
+		}else{
+			System.out.println("Not Amstrong");
+		}
+	}
