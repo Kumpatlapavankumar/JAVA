c6ad6a7edff3ed1d3295c12d770b329d0e29b1f4
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-11-23 21:27:05 +0530

    Create Program to print pattern.java

---
+++ Program to print pattern.java
@@ -0,0 +1,13 @@
+public class pattern{  
+public static void main(String[] args){  
+int lines=10;  
+int i=1;  
+int j;  
+for(i=1;i<=lines;i++){// this loop is used to print the lines  
+for(j=1;j<=i;j++){// this loop is used to print lines  
+System.out.print(i*j+" ");  
+}  
+System.out.println("");  
+}  
+}  
+}  
