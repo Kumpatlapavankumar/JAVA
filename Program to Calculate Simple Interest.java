2a51aa88d47fa4cd00258d117d9b628883a6f19b
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-11-28 21:03:04 +0530

    Create Program to Calculate Simple Interest.java

---
+++ Program to Calculate Simple Interest.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+
+class simpleinterest{
+	public static void main(String args[])
+	{
+		// We can change values here for
+		// different inputs
+		float P = 1, R = 1, T = 1;
+
+		/* Calculate simple interest */
+		float SI = (P * T * R) / 100;
+		System.out.println("Simple interest = " + SI);
+	}
+}
+
+// This code is contributed by Anant Agarwal.
