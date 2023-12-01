c8003e0e7f2874b2d2422061fcb4008291068a1f
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-12-01 21:07:44 +0530

    Update and rename Java Stdin and Stdout I.java to Java Stdin and Stdout.java

--- Java Stdin and Stdout I.java
+++ Java Stdin and Stdout I.java
@@ -5,14 +5,14 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
         int b = scan.nextInt();
         int c = scan.nextInt();
+        char d=scan.nextChar();
+        String name=scan.nextLine();
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        // Complete this line
-        // Complete this line
+        System.out.println(d);
+        System.out.println(name);
     }
 }
